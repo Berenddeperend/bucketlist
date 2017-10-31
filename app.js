87114a1d35a693d15676260af0b2566d79527a18
Berend Swennenhuis <berendswennenhuis@gmail.com>
2017-10-31 19:12:57 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// We will declare all our dependencies here
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const config = require('./config/database');
+const bucketlist = require('./controllers/bucketlist');
+
+
+
+
+
+//Initialize our app variable
+const app = express();
+
+//Declaring Port
+const port = 4000;
+
+
+//Middleware for CORS
+app.use(cors());
+
+//Middleware for bodyparsing using both json and urlencoding
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+/*express.static is a built in middleware function to serve static files.
+ We are telling express server public folder is the place to look for the static files
+*/
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routing 
+app.get('/', (req,res) => {
+    res.send("Welkom.");
+});
+
+app.use('/bucketlist', bucketlist);
+
+// Fire 'em up
+app.listen(port, () => {
+    console.log(`Starting the server at port ${port}`);
+});
+
+mongoose.connect(config.database, {
+    useMongoClient: true
+});
